3e45d242f374d2311dab988a98cf2aa45c93fd23
Maximus0904 <marcio.santo.junior@escola.pr.gov.br>
2025-09-19 16:21:48 -0300

    Create Ativ4.py

---
+++ Ativ4.py
@@ -0,0 +1,6 @@
+print("Exercício D")
+senha_Principal = '1234'
+senha_Tentativa = input("Digite a senha = ")
+while senha_Tentativa != senha_Principal:
+    senha_Tentativa = input("Tente Novamente = ")
+print("Acesso Permitido")
