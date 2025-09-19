7f2fcf8dd0d43af2dfab8db3ef5e448773e6cd1d
Maximus0904 <marcio.santo.junior@escola.pr.gov.br>
2025-09-19 16:10:44 -0300

    Create AtivC.py

---
+++ AtivC.py
@@ -0,0 +1,7 @@
+print("Exercício C")
+numero_tabuada = int(input("Número para a tabuada = "))
+i = 0
+print(f"Tabuada do {numero_tabuada}:")
+for _ in range(1,11):
+    i += 1
+    print(f" . {numero_tabuada} x {i} = {numero_tabuada*i}")
